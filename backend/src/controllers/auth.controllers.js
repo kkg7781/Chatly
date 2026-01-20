4b38e14c7592efd04c90bcfd5a512ce32984acf4
Karan gupta <karangupta7481@gmail.com>
2026-01-20 11:01:04 +0530

    Add login and logout functionality

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -2,6 +2,7 @@ import User from "../models/user.models.js";
 import bcrypt from "bcryptjs"
 import { generateToken } from "../lib/utils.js";
 
+
 export const signup=async(req,res)=>{
 
 const{fullName, email, password}=req.body
@@ -54,4 +55,42 @@ else{
       return res.status(500).json({ message: error.message });
 }
 
+}
+export const login =async(req, res)=>{
+    
+    try {
+        const {fullName,email, password}=req.body
+    const user= await User.findOne({email})
+    if(!user){
+        return res.status(400).json({message: "Invalid Credentials "})
+    }
+    if(!password){
+        return res.status(400).json({message:"Password field can't be empty"})
+    }
+    const comparePassword= await bcrypt.compare(password, user.password)
+    if(!comparePassword){
+        return res.status(400).json({message : "Invalid Credentials"})
+    }
+    generateToken(user._id, res)
+    return  res.status(200).json({
+        id:user._id,
+        fullName:user.fullName,
+        email:user.email,
+        profilePic:user.profilePic
+
+    })
+
+
+
+    } catch (error) {
+        console.error("Error in login controller", error)
+        res.status(500).json({messgae:"Internal Server Error"})
+        
+    }
+
+
+}
+export const logout=async(req, res)=>{
+    res.cookie("jwt", "", {maxAge :0});
+    res.status(200).json({message:"Logged Out Successfully"})
 }
