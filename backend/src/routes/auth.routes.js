c170bd62f56b1cb2136fe5e82bc5a0e6ec3b48e2
Karan gupta <karangupta7481@gmail.com>
2026-01-22 01:32:05 +0530

    Add Arcjet protection middleware and update dependencies

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,9 +2,11 @@ import express from "express"
 import { signup,login ,logout, updateProfile } from "../controllers/auth.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 const router=express.Router();
+router.use(arcjetProtection)
 router.post("/signup",signup)
-router.post("/login", login)
+router.post("/login", arcjetProtection,login)
 // protected routes
 router.post("/logout",verifyJWT, logout)
 router.put(
