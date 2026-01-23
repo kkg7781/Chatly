b75a1de97ca596ca734f75db154d4916699eed3b
Karan gupta <karangupta7481@gmail.com>
2026-01-23 18:53:21 +0530

    Implement messaging features: add sendMessages other mssg functions

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -1,6 +1,10 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.models.js";
 import User from "../models/user.models.js";
 
+//getAllconatcts controller
+// Fetch all users except the currently logged-in user
+// - Used to show contacts list in chat sidebar
 export const getAllContacts=async(req,res)=>{
 try {
     const loggedInUser= req.user._id
@@ -14,3 +18,88 @@ try {
 }
 
 }
+//for message controller
+// - Fetch all chat messages between
+//   logged-in user and selected user
+// - Used when opening a chat window
+
+export const getMessagesByUserId=async(req,res)=>{
+    try {
+        const myId=req.user._id //loggedIn user id
+        const {id:userToChatId}=req.params // extract user to whom i have to send message from url rename it for clarity
+
+        const messages=await Message.find({
+            $or :[
+                { senderId:myId,  receiverId:userToChatId}, // 1. I sent message to selected user OR
+   
+    
+                {  senderId:userToChatId, receiverId:myId } // // 2. Selected user sent message to me
+            ]
+        })
+        return res.status(200).json(messages)
+
+    } catch (error) {
+        console.log("Error in getMessageByUserId",error.message)
+    res.status(500).json({message:"Internal Server Error"})
+    }
+}
+//  for send message
+ export const sendMessages=async(req,res)=>{
+   try {
+     const {text}=req.body
+     const image=req.file  // when using multer remember that image will be there in req.file not req.body
+     const {id: receiverId}=req.params
+     const senderId=req.user._id
+     let imageUrl=null
+     if(image){
+ const uploadResponse =await cloudinary.uploader.upload(image.path,
+        {
+          folder: "chat-images"
+        });
+        imageUrl = uploadResponse.secure_url;
+     }
+
+     //create a new message
+     const newMessage= await Message.create(
+        {senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        }
+     )
+     return res.status(201).json(newMessage);
+   } catch (error) {
+    console.log("Error in sendMessages:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+    
+   }
+ }
+
+ 
+export const getChatPartners = async(req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    // find all the messages where the logged-in user is either sender or receiver
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    });
+
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId.toString()
+            ? msg.receiverId.toString()
+            : msg.senderId.toString()
+        )
+      ),
+    ];
+
+    const chatPartners = await User.find({ _id: { $in: chatPartnerIds } }).select("-password");
+
+    res.status(200).json(chatPartners);
+  } catch (error) {
+    console.error("Error in getChatPartners: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { getAllContacts } from "../controllers/message.controllers.js";
+import { getAllContacts,getChatPartners,getMessagesByUserId, sendMessages} from "../controllers/message.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=express.Router()
 //protected route
-//router.get("/chats", getChatPatrners)
+
 router.get("/contacts",verifyJWT,getAllContacts)
-//router.get("/:id", getMessagesByUserId)
-//router.post("/send/:id", sendMessages)
+router.get("/chats", verifyJWT,getChatPartners)
+
+router.get("/:id",verifyJWT, getMessagesByUserId)
+
+router.post("/send/:id",verifyJWT,  
+    upload.single("image"),  //multer runs first
+ sendMessages)
+
 export default router
