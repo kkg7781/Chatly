839f000f6b28089f268c6aca0feab50391ee5b62
Karan gupta <karangupta7481@gmail.com>
2026-01-23 18:58:24 +0530

    Add validation checks in getMessagesByUserId , fix message model

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -50,6 +50,18 @@ export const getMessagesByUserId=async(req,res)=>{
      const image=req.file  // when using multer remember that image will be there in req.file not req.body
      const {id: receiverId}=req.params
      const senderId=req.user._id
+
+     //few checks added
+     if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
+    }
+    if (senderId.equals(receiverId)) {
+      return res.status(400).json({ message: "Cannot send messages to yourself." });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found." });
+    }
      let imageUrl=null
      if(image){
  const uploadResponse =await cloudinary.uploader.upload(image.path,
