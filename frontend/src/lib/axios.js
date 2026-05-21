e97ca839432e1f580203b1a484ff0fe7b9f2e39b
Karan gupta <karangupta7481@gmail.com>
2026-05-21 16:03:34 +0530

    add backend url in production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://chatly-fm17.onrender.com",
   withCredentials: true, // if using cookies / JWT
 });
 
